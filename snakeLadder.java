b414e9f6d4326f1cfe02e36aab703844fad6225b
amitkrmaharana <ak657harley@gmail.com>
2021-02-09 13:40:41 +0530

    Added: Rolled the dice to get a face number

--- snakeLadder.java
+++ snakeLadder.java
@@ -3,5 +3,7 @@ public class snakeLadder {
 	public static void main(String[] args) {
 		int player_position = 0;
 		System.out.println("Welcome to Snake and Ladder Simulation Program");
+		double dice_roll = Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println("The dice rolled "+(int)dice_roll);
 	}
 }
