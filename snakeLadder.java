e5a9875bf5ef5bbf33bf602c8569367c81c8a476
amitkrmaharana <ak657harley@gmail.com>
2021-02-09 15:12:32 +0530

    Refactor: continues till player position reaches 100 or more

--- snakeLadder.java
+++ snakeLadder.java
@@ -6,16 +6,19 @@ public class snakeLadder {
 	public static void main(String[] args) {
 		int player_position = 0;
 		System.out.println("Welcome to Snake and Ladder Simulation Program");
-		double dice_roll = Math.floor(Math.random() * 10) % 6 + 1;
-		System.out.println("The dice rolled "+(int)dice_roll);
-		double play_check = Math.floor(Math.random() *10) % 3;
-		switch((int)play_check) {
-			case IS_LADDER:
-				player_position+=dice_roll;
-				break;
-			case IS_SNAKE:
-				player_position-=dice_roll;
-				break;
+		while (player_position < 100) {
+			double dice_roll = Math.floor(Math.random() * 10) % 6 + 1;
+			double play_check = Math.floor(Math.random() *10) % 3;
+			switch((int)play_check) {
+				case IS_LADDER:
+					player_position+=dice_roll;
+					break;
+				case IS_SNAKE:
+					player_position-=dice_roll;
+					if (player_position < 0)
+						player_position = 0;
+					break;
+			}
 		}
 		System.out.println("Player position is "+player_position);
 	}
