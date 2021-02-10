a4e250e8ab790a016b0b0c5b21964ec58f582093
Amit Kumar Maharana <77839157+amitkrmaharana@users.noreply.github.com>
2021-02-10 11:16:03 +0530

    Added: counted the dice rolled

--- snakeLadder.java
+++ snakeLadder.java
@@ -9,20 +9,20 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake and Ladder Simulation Program");
 		while (player_position < 100) {
 			double dice_roll = Math.floor(Math.random() * 10) % 6 + 1;
-			double play_check = Math.floor(Math.random() *10) % 3;
+			double play_check = Math.floor(Math.random() * 10) % 3;
 			dice_roll_count++;
 			switch((int)play_check) {
 				case IS_LADDER:
-					player_position+=dice_roll;
+					player_position += dice_roll;
 					break;
 				case IS_SNAKE:
-					player_position-=dice_roll;
+					player_position -= dice_roll;
 					if (player_position < 0)
 						player_position = 0;
 					break;
 			}
 			if (player_position > 100)
-				player_position-=dice_roll;
+				player_position -= dice_roll;
 			System.out.println("You are at position "+player_position);
 		}
 		System.out.println("********YOU WON********");
