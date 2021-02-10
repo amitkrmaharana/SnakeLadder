882c374429e09d34d830955702329dc895463035
Amit Kumar Maharana <77839157+amitkrmaharana@users.noreply.github.com>
2021-02-10 11:17:38 +0530

    Refactored: 2 players play the game

--- snakeLadder.java
+++ snakeLadder.java
@@ -12,27 +12,27 @@ public class SnakeLadder {
 			double dice_roll1 = Math.floor(Math.random() * 10) % 6 + 1;
 			double play_check1 = Math.floor(Math.random() *10) % 3;
 			double dice_roll2 = Math.floor(Math.random() * 10) % 6 + 1;
-                        double play_check2 = Math.floor(Math.random() *10) % 3;
+                        double play_check2 = Math.floor(Math.random() * 10) % 3;
 			dice_roll_count++;
 			switch((int)play_check1) {
 				case IS_LADDER:
-					player1_position+=dice_roll1;
+					player1_position += dice_roll1;
 					break;
 				case IS_SNAKE:
-					player1_position-=dice_roll1;
+					player1_position -= dice_roll1;
 					if (player1_position < 0)
 						player1_position = 0;
 					break;
 			}
 			if (player1_position > 100)
-				player1_position-=dice_roll1;
+				player1_position -= dice_roll1;
 			System.out.println("Player1 is at position "+player1_position);
 			switch((int)play_check2) {
                                 case IS_LADDER:
-                                        player2_position+=dice_roll2;
+                                        player2_position += dice_roll2;
                                         break;
                                 case IS_SNAKE:
-                                        player2_position-=dice_roll2;
+                                        player2_position -= dice_roll2;
                                         if (player2_position < 0)
                                                 player2_position = 0;
                                         break;
