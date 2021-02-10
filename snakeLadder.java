18a22dcb740a4ad825779d6e37bbbbc81925c5b8
Amit Kumar Maharana <77839157+amitkrmaharana@users.noreply.github.com>
2021-02-10 11:09:08 +0530

    Refactored: Win at exact 100

--- snakeLadder.java
+++ snakeLadder.java
@@ -8,19 +8,19 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake and Ladder Simulation Program");
 		while (player_position < 100) {
 			double dice_roll = Math.floor(Math.random() * 10) % 6 + 1;
-			double play_check = Math.floor(Math.random() *10) % 3;
+			double play_check = Math.floor(Math.random() * 10) % 3;
 			switch((int)play_check) {
 				case IS_LADDER:
-					player_position+=dice_roll;
+					player_position += dice_roll;
 					break;
 				case IS_SNAKE:
-					player_position-=dice_roll;
+					player_position -= dice_roll;
 					if (player_position < 0)
 						player_position = 0;
 					break;
 			}
 			if (player_position > 100)
-				player_position-=dice_roll;
+				player_position -= dice_roll;
 		}
 		System.out.println("*******YOU WON*******");
 		System.out.println("Player position is "+player_position);
